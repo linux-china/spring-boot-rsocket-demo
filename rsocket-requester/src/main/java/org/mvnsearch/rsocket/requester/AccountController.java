17811baba8619d394af5db6be97d6107d0bef6af
linux_china <libing.chen@gmail.com>
2019-04-16 10:38:15 +0800

    account controller

--- rsocket-requester/src/main/java/org/mvnsearch/rsocket/requester/AccountController.java
+++ rsocket-requester/src/main/java/org/mvnsearch/rsocket/requester/AccountController.java
@@ -1,8 +1,8 @@
 package org.mvnsearch.rsocket.requester;
 
 import org.mvnsearch.account.Account;
+import org.mvnsearch.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +18,10 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/account")
 public class AccountController {
     @Autowired
-    private RSocketRequester rsocketRequester;
+    private AccountService accountService;
 
     @GetMapping("/{id}")
     public Mono<Account> show(@PathVariable Integer id) {
-        return rsocketRequester.route("findById").data(1).retrieveMono(Account.class);
+        return accountService.findById(id);
     }
 }
