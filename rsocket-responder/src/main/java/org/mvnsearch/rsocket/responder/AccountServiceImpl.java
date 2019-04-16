a2b2b9fa8470ea41046bc8540867ae372acbd557
linux_china <libing.chen@gmail.com>
2019-04-16 16:40:15 +0800

    add findAll

--- rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/AccountServiceImpl.java
+++ rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package org.mvnsearch.rsocket.responder;
 import org.mvnsearch.account.Account;
 import org.mvnsearch.account.AccountService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -15,5 +16,10 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public Mono<Account> findById(Integer id) {
         return Mono.just(new Account(id, "nick:" + id));
+    }
+
+    @Override
+    public Flux<Account> findAll() {
+        return Flux.just(new Account(1, "Jackie"), new Account(2, "Tom"));
     }
 }
