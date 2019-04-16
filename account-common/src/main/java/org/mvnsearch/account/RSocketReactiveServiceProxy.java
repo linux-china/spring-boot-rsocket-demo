abd1c555ffb0faf45e5e01552aa432dca6e06553
linux_china <libing.chen@gmail.com>
2019-04-16 11:39:35 +0800

    fix return data type bug

--- account-common/src/main/java/org/mvnsearch/account/RSocketReactiveServiceProxy.java
+++ account-common/src/main/java/org/mvnsearch/account/RSocketReactiveServiceProxy.java
@@ -5,6 +5,8 @@ import reactor.core.publisher.Mono;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.time.Duration;
 
 /**
@@ -23,15 +25,22 @@ public class RSocketReactiveServiceProxy implements InvocationHandler {
         this.timeout = timeout;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         String routeKey = serviceFullName + "." + method.getName();
+        Class returnDataType = null;
+        Type genericReturnType = method.getGenericReturnType();
+        if (genericReturnType != null) {
+            ParameterizedType aType = (ParameterizedType) method.getGenericReturnType();
+            returnDataType = (Class) aType.getActualTypeArguments()[0];
+        }
         if (method.getReturnType().isAssignableFrom(Mono.class)) {
-            return rsocketRequester.route(routeKey).data(args[0]).retrieveMono(args[0].getClass()).timeout(timeout);
+            return rsocketRequester.route(routeKey).data(args[0]).retrieveMono(returnDataType).timeout(timeout);
         } else if (method.getReturnType().isAssignableFrom(Mono.class)) {
-            return rsocketRequester.route(routeKey).data(args[0]).retrieveFlux(args[0].getClass()).timeout(timeout);
+            return rsocketRequester.route(routeKey).data(args[0]).retrieveFlux(returnDataType).timeout(timeout);
         } else {
-            return rsocketRequester.route(routeKey).data(args).send().timeout(timeout);
+            return rsocketRequester.route(routeKey).data(args[0]).send().timeout(timeout);
         }
     }
 }
