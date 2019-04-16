bfb664fd57d54e926652adf415d124937187fca2
linux_china <libing.chen@gmail.com>
2019-04-16 09:50:56 +0800

    rsocket requester app

---
+++ rsocket-requester/src/main/java/org/mvnsearch/rsocket/requester/RSocketRequesterApp.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.rsocket.requester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * rsocket requester app
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class RSocketRequesterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RSocketRequesterApp.class, args);
+    }
+}
