7604c0fab03f2fc34881db4821a13e7d3ea237c8
linux_china <libing.chen@gmail.com>
2019-10-07 14:18:27 -0700

    polishing

--- rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/AccountRSocketController.java
+++ rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/AccountRSocketController.java
@@ -19,17 +19,17 @@ public class AccountRSocketController {
     @Autowired
     private AccountService accountService;
 
-    @MessageMapping("org_mvnsearch_account_AccountService_findById")
+    @MessageMapping("org.mvnsearch.account.AccountService_findById")
     public Mono<Account> findById(Integer id) {
         return accountService.findById(id);
     }
 
-    @MessageMapping("org_mvnsearch_account_AccountService_findById_{id}")
+    @MessageMapping("org.mvnsearch.account.AccountService_findById_{id}")
     public Mono<Account> findById2(@DestinationVariable Integer id) {
         return accountService.findById(id);
     }
 
-    @MessageMapping("org_mvnsearch_account_AccountService_findAll")
+    @MessageMapping("org.mvnsearch.account.AccountService_findAll")
     public Flux<Account> findAll() {
         return accountService.findAll();
     }
