3eccf9edadf04e4e063bba096ccd2b3d03f70375
linux_china <libing.chen@gmail.com>
2020-01-28 15:35:53 -0800

    RSocket exposed

--- rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/AccountServiceImpl.java
+++ rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/AccountServiceImpl.java
@@ -2,23 +2,29 @@ package org.mvnsearch.rsocket.responder;
 
 import org.mvnsearch.account.Account;
 import org.mvnsearch.account.AccountService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
- * account service implementation
+ * account service implementation with RSocket exposed
  *
  * @author linux_china
  */
 @Service
+@Controller
+@MessageMapping("org.mvnsearch.account.AccountService")
 public class AccountServiceImpl implements AccountService {
     @Override
+    @MessageMapping("findById")
     public Mono<Account> findById(Integer id) {
         return Mono.just(new Account(id, "nick:" + id));
     }
 
     @Override
+    @MessageMapping("findAll")
     public Flux<Account> findAll() {
         return Flux.just(new Account(1, "Jackie"), new Account(2, "Tom"));
     }
