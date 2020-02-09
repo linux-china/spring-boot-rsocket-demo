e85eda4014bd8cbe16ce8aebe76f3e5f334214ff
linux_china <libing.chen@gmail.com>
2020-02-09 13:49:50 -0800

    User Service implementation

---
+++ rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/UserServiceImpl.java
@@ -0,0 +1,28 @@
+package org.mvnsearch.rsocket.responder;
+
+import com.github.javafaker.Faker;
+import org.mvnsearch.account.User;
+import org.mvnsearch.account.UserService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+/**
+ * User Service implementation
+ *
+ * @author linux_china
+ */
+@Controller
+@MessageMapping("org.mvnsearch.account.UserService")
+public class UserServiceImpl implements UserService {
+    private Faker faker = new Faker();
+
+    @MessageMapping("findById")
+    public User findById(Integer id) {
+        User user = new User();
+        user.setId(id);
+        user.setNick(faker.name().name());
+        user.setPhone(faker.phoneNumber().cellPhone());
+        user.setEmail(faker.internet().emailAddress());
+        return user;
+    }
+}
