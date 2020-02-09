55f72abd15e3dc1e270f5fd75ec999ed94ebf2d2
linux_china <libing.chen@gmail.com>
2020-02-09 14:05:01 -0800

    Polishing

--- account-common/src/main/java/org/mvnsearch/account/UserService.java
+++ account-common/src/main/java/org/mvnsearch/account/UserService.java
@@ -1,10 +1,12 @@
 package org.mvnsearch.account;
 
+import reactor.core.publisher.Mono;
+
 /**
  * User service
  *
  * @author linux_china
  */
 public interface UserService {
-    User findById(Integer id);
+    Mono<User> findById(Integer id);
 }
--- rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/UserServiceImpl.java
+++ rsocket-responder/src/main/java/org/mvnsearch/rsocket/responder/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.mvnsearch.account.User;
 import org.mvnsearch.account.UserService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
 /**
  * User Service implementation
@@ -17,12 +18,12 @@ public class UserServiceImpl implements UserService {
     private Faker faker = new Faker();
 
     @MessageMapping("findById")
-    public User findById(Integer id) {
+    public Mono<User> findById(Integer id) {
         User user = new User();
         user.setId(id);
         user.setNick(faker.name().name());
         user.setPhone(faker.phoneNumber().cellPhone());
         user.setEmail(faker.internet().emailAddress());
-        return user;
+        return Mono.just(user);
     }
 }
